80fd981444947fd19030516b2ec7d4324ffc84af
Stephen Weil <stephen.weil@gmail.com>
2021-03-24 23:58:15 -0500

    Add letter opener, fix rest of login flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   root to: "leagues#index"
 
   post '/users', to: 'users#create', as: 'register'
+  post '/sessions', to: 'sessions#create', as: 'login'
+
+  devise_for :users
+
+  # put this after devise because of /users/sign_in
+  get '/users/:user_id', to: 'users#show', as: 'user_profile'
 end
