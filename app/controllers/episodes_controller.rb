a0909764b8afa2a3ef2b297b202effc136cf704c
Stephen Weil <stephen.weil@gmail.com>
2021-06-03 22:13:22 -0500

    Nil handle empty categories

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -43,14 +43,14 @@ class EpisodesController < ApplicationController
     episode.reload
     episode.episode_chefs.select { |ec| params[:qf_winner].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(qf_win: true) } if params[:qf_winner].is_a?(Array)
     episode.episode_chefs.select { |ec| params[:qf_fav].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(qf_fav: true) } if params[:qf_fav].is_a?(Array)
-    episode.episode_chefs.select { |ec| params[:elim_winner].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(elim_win: true) }
-    episode.episode_chefs.select { |ec| params[:elim_fav].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(elim_top: true) }
-    episode.episode_chefs.select { |ec| params[:elim_bottom].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(elim_bottom: true) }
-    episode.episode_chefs.select { |ec| params[:lck_winner].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(lck_win: true) }
+    episode.episode_chefs.select { |ec| params[:elim_winner].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(elim_win: true) } if params[:elim_winner].is_a?(Array
+    episode.episode_chefs.select { |ec| params[:elim_fav].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(elim_top: true) } if params[:elim_fav].is_a?(Array
+    episode.episode_chefs.select { |ec| params[:elim_bottom].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(elim_bottom: true) } if params[:elim_bottom].is_a?(Array)
+    episode.episode_chefs.select { |ec| params[:lck_winner].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(lck_win: true) } if params[:lck_winner].is_a?(Array)
     episode.episode_chefs.select { |ec| params[:finale]&.include?(ec.chef_id) }&.each { |ec| ec.update_attributes(finale: true) }
     episode.episode_chefs.detect { |ec| ec.chef_id == params[:champ] }&.update_attributes(champ: true)
     episode.episode_chefs.detect { |ec| ec.chef_id == params[:lck_champ] }&.update_attributes(lck_champ: true)
-    episode.episode_chefs.select { |ec| params[:eliminated].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(eliminated: true) }
+    episode.episode_chefs.select { |ec| params[:eliminated].include?(ec.chef_id) }&.each { |ec| ec.update_attributes(eliminated: true) } if params[:eliminated].is_a?(Array)
 
     update_total_points!
     send_notification_email!
